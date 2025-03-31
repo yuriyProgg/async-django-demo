8677528157d06bd64650fbb17404582fda9d8311
Yuri Masalov <masalov.09@bk.ru>
2025-03-31 23:21:17 +0600

    Refactor build process: replace install.sh with poetry install in build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+poetry install
 python3 manage.py test
 python3 manage.py collectstatic
 python3 manage.py compress --force
--- install.sh
+++
@@ -1,3 +0,0 @@
-apt update -y
-apt install -y python3-full sqlite3 libsqlite3-dev python3-poetry
-poetry install --no-root
